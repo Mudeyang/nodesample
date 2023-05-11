1424a1cbc573580ec9c5eebb8e96eacf3e685fa3
Mudeyang <mudeyangsimba@gmail.com>
2023-05-11 12:17:38 +0300

    first commit

---
+++ person.js
@@ -0,0 +1,5 @@
+const person = {
+  name: "Mudey Abdi",
+  age: 20,
+};
+module.exports = person;
---
+++ reference/os_demo.js
@@ -0,0 +1,22 @@
+const os = require("os");
+
+// platform
+console.log(os.platform());
+
+//CPU architecture
+console.log(os.arch());
+
+// CPU Core Info
+console.log(os.cpus());
+
+// Free Memory
+console.log(os.freemem());
+
+// Total Memory
+console.log(os.totalmem());
+
+//Home dir
+console.log(os.homedir());
+
+//uptime
+console.log(os.uptime());
